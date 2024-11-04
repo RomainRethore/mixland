5f7446e8322d7a53b11ccec293c95dd4fb57c7a0
RomainR <romain.rethore@gmail.com>
2024-11-04 20:32:03 +0100

    FEAT : mix crud         Création d'un formulaire d'upload d'un mix et modification de ce formulaire pour permettre l'ipload d'un mp3         Création d'un Controller pour gérer le formulaire

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Mix;
+use App\Form\MixFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class MixController extends AbstractController
 {
@@ -15,6 +22,61 @@ class MixController extends AbstractController
 
         return $this->render('mix/index.html.twig', [
             'controller_name' => 'MixController',
+        ]);
+    }
+
+    #[Route('mix/new', name: 'app_mix_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/mixes/')] string $mixesDirectory): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $mix = new Mix();
+
+        $form = $this->createForm(MixFormType::class, $mix, [
+            'attr' => ['id' => 'mix-form']
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $mixFile = $form->get('audio')->getData();
+
+            if ($mixFile) {
+
+                $originalFilename = pathinfo($mixFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $slugger->slug($originalFilename);
+
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $mixFile->getClientOriginalExtension();
+
+                $user = $this->getUser();
+
+                $userDirectory = $mixesDirectory . '/' . $user->getId();
+
+                if (!is_dir($userDirectory)) {
+                    mkdir($userDirectory, 0777, true);
+                }
+
+                try {
+                    $mixFile->move($userDirectory, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                $mix->setAudio($newFilename);
+            }
+
+            $entityManager->persist($mix);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Mix created successfully');
+
+            return $this->redirectToRoute('app_mix');
+        }
+
+        return $this->render('mix/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
---
+++ src/Form/MixFormType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mix;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class MixFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('title')
+            ->add('description')
+            ->add('tracklist')
+            ->add('audio')
+            ->add('audio', FileType::class, [
+                'label' => 'File (MP3 file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the MP3 file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using attributes
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10024k',
+                        'mimeTypes' => [
+                            'audio/mpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid mp3 file',
+                    ])
+                ],
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Upload Mix',
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Mix::class,
+        ]);
+    }
+}
